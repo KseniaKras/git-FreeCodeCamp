79266399c7cea6c65e8098d239e4739fe3e29445
unknown <KseniaKras>
2021-12-17 21:21:27 +0300

    added new file BasicJS-25-30.js

---
+++ BasicJS-25-30.js
@@ -0,0 +1,20 @@
+// 25. Escaping Literal Quotes in StringsPassed
+		const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+// 26. Quoting Strings with Single QuotesPassed
+		const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// 27. Escape Sequences in StringsPassed
+		const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+// 28. Concatenating Strings with Plus OperatorPassed
+		const myStr = "This is the start. "+ "This is the end."; // Change this line
+
+// 29. Concatenating Strings with the Plus Equals Operator
+		let myStr = "This is the first sentence. ";
+		myStr += "This is the second sentence.";
+
+// 30. Constructing Strings with Variables
+		// Only change code below this line
+		const myName = "Ksenia";
+		const myStr = "My name is " + myName + " and I am well!";
