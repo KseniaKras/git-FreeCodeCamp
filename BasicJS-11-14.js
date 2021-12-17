9ba3148aa081856df31a1215b0f581b09221778f
unknown <KseniaKras>
2021-12-17 21:00:44 +0300

    added new file BasicJS-11-14.js

---
+++ BasicJS-11-14.js
@@ -0,0 +1,11 @@
+// 11. Add Two Numbers with JavaScriptPassed
+		const sum = 10 + 10;
+
+// 12. Subtract One Number from Another with JavaScriptPassed
+		const difference = 45 - 33;
+
+// 13. Multiply Two Numbers with JavaScript
+		const product = 8 * 10; 
+
+// 14. Divide One Number by Another with JavaScript
+		const quotient = 66 / 33;
