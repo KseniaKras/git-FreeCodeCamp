7f8e6e924b614e1fd2adddc300b0f5512870ecc7
unknown <KseniaKras>
2021-12-17 21:02:38 +0300

    added new file BasicJS-15-20.js

---
+++ BasicJS-15-20.js
@@ -0,0 +1,25 @@
+// 15. Increment a Number with JavaScriptPassed
+		let myVar = 87;
+      // Only change code below this line
+		myVar++;
+
+// 16. Decrement a Number with JavaScript
+		let myVar = 11;
+		// Only change code below this line
+		myVar--;
+
+// 17. Create Decimal Numbers with JavaScriptPassed
+		const ourDecimal = 5.7;
+		// Only change code below this line
+		const myDecimal = 5.7;
+
+// 18. Multiply Two Decimals with JavaScriptPassed
+		const product = 2.0 * 2.5;
+
+// 19. Divide One Decimal by Another with JavaScriptPassed
+		const quotient = 4.4 / 2.0; // Change this line
+
+// 20. Finding a Remainder in JavaScriptPassed
+		const remainder = 11 % 3;
+
+		
