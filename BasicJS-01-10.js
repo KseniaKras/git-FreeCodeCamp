54039df4ecf516e33c332c23dd665fb2318b75e5
unknown <KseniaKras>
2021-12-17 20:38:45 +0300

    added new file Basic-01-10.js

---
+++ BasicJS-01-10.js
@@ -0,0 +1,61 @@
+// 1. Comment Your JavaScript Code
+      // hello
+      /* hello
+        world */
+  
+
+ // 2. Declare JavaScript VariablesPassed
+      var myName;
+
+ // 3. Storing Values with the Assignment OperatorPassed
+      // Setup
+      var a;
+      // Only change code below this line
+      a = 7;
+
+// 4. Assigning the Value of One Variable to AnotherPassed
+     // Setup
+       var a;
+       a = 7;
+       var b;
+       b = a;
+       // Only change code below this line
+
+// 5. Initializing Variables with the Assignment Operator
+      var a = 9;
+
+// 6. Declare String VariablesPassed
+      var myFirstName = "myFirstName";
+      var myLastName = "myLastName";
+
+// 7. Understanding Uninitialized VariablesPassed
+      // Only change code below this line
+      var a = 5;
+      var b = 10;
+      var c = "I am a";
+      // Only change code above this line
+
+      a = a + 1;
+      b = b + 5;
+      c = c + " String!";
+
+// 8. Understanding Case Sensitivity in Variables
+      // Variable declarations
+      var studlyCapVar;
+      var properCamelCase;
+      var titleCaseOver;
+
+      // Variable assignments
+      studlyCapVar = 10;
+      properCamelCase = "A String";
+      titleCaseOver = 9000;
+
+// 9. Explore Differences Between the var and let KeywordsPassed
+      let catName = "Oliver";
+      let catSound = "Meow!";
+
+// 10. Declare a Read-Only Variable with the const KeywordPassed
+      const FCC = "freeCodeCamp"; // Change this line
+      let fact = "is cool!"; // Change this line
+      fact = "is awesome!";
+      console.log(FCC, fact); // Change this line
