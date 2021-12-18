afc2afc03f88e8fb9eaf8e2bc7cbde07beb0b444
unknown <KseniaKras>
2021-12-18 15:03:40 +0300

    added new file BasicJS-41-45.js

---
+++ BasicJS-41-45.js
@@ -0,0 +1,34 @@
+// 41. Use Bracket Notation to Find the Nth Character in a String
+		// Setup
+		const lastName = "Lovelace";
+
+		// Only change code below this line
+		const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// 42. Use Bracket Notation to Find the Last Character in a String
+		// Setup
+		const lastName = "Lovelace";
+
+		// Only change code below this line
+		const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+
+// 43. Use Bracket Notation to Find the Nth-to-Last Character in a String
+		// Setup
+		const lastName = "Lovelace";
+
+		// Only change code below this line
+		const secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
+
+// 44. Word Blanks,
+		const myNoun = "dog";
+		const myAdjective = "big";
+		const myVerb = "ran";
+		const myAdverb = "quickly";
+
+		// Only change code below this line
+		const wordBlanks = "It was really " + myAdjective +" " + myNoun + " and it " + myVerb + " so " + myAdverb +"." ; // Change this line
+		// Only change code above this line
+
+// 45. Store Multiple Values in one Variable using JavaScript Arrays
+		// Only change code below this line
+		const myArray = ["string ", 1];
