b99f19b633288a8722a5d91ffce0e31e14900b27
unknown <KseniaKras>
2021-12-18 15:02:27 +0300

    added new file BasicJS-31-40

---
+++ BasicJS-31-40.js
@@ -0,0 +1,31 @@
+// 31. Appending Variables to Strings
+		// Change code below this line
+		const someAdjective = "useful, engrossing and difficult process!";
+		let myStr = "Learning to code is ";
+		myStr += someAdjective;
+
+// 32. Find the Length of a String
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// 33. Only change code below this line
+lastNameLength = lastName.length;
+
+// 34. Use Bracket Notation to Find the First Character in a String
+		// Setup
+		let firstLetterOfLastName = "";
+		const lastName = "Lovelace";
+
+		// Only change code below this line
+		firstLetterOfLastName = lastName[0]; // Change this line
+
+// 35. Understand String Immutability
+		// Setup
+		let myStr = "Jello World";
+
+		// Only change code below this line
+		myStr = "Hello World"; // Change this line
+		// Only change code above this line
+
+		
