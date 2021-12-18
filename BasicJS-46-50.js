c3f12de0e8f1a322b23e963b6a992d3dadba942d
unknown <KseniaKras>
2021-12-18 15:04:39 +0300

    added new file BasicJS-46-50.js

---
+++ BasicJS-46-50.js
@@ -0,0 +1,29 @@
+// 46. Nest one Array within Another Array
+		// Only change code below this line
+		const myArray = [["First Array",5],["Second Array",7]];
+
+// 47. Access Array Data with Indexes
+		const myArray = [50, 60, 70];
+		myArray[0];
+		const myData = myArray[0];
+
+// 48. Modify Array Data With Indexes
+		// Setup
+		const myArray = [18, 64, 99];
+		// Only change code below this line
+		myArray[0] = 45;
+
+// 49. Access Multi-Dimensional Arrays With Indexes
+		const myArray = [
+			[1, 2, 3],
+			[4, 5, 6],
+			[7, 8, 9],
+			[[10, 11, 12], 13, 14],
+		];
+		const myData = myArray[2][1];
+
+// 50. Manipulate Arrays With push()
+		// Setup
+		const myArray = [["John", 23], ["cat", 2]];
+		// Only change code below this line
+		myArray.push(["dog", 3]);
